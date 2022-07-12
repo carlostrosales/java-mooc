1ba5d702020d5237e01456a9964948f1e04b6785
carlostrosales <rosales.carlost@gmail.com>
2022-07-12 14:10:34 -0500

    Completed Exercise 6_13 and uploaded not yet completed exercises for part 07

--- mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,40 @@
 
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class ExerciseManagementTest {
-
+    
+    private ExerciseManagement management;
+    
+    @Test
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+    
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        ExerciseManagement management = new ExerciseManagement();
+        assertEquals(0, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExerciseIsInList() {
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+    
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
 }
