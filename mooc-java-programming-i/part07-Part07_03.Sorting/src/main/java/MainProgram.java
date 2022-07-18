4d4c02de3d84aa163405afdb2a7e4c6b409fef12
carlostrosales <rosales.carlost@gmail.com>
2022-07-18 13:31:56 -0500

    Implemented sorting algorithms from scratch using helper methods and also seperately with java in-built array methods and collections

--- mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,64 @@
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(numbers);
     }
 
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length;i++) {
+            System.out.println(Arrays.toString(array));
+            int smallest = indexOfSmallestFrom(array, i);
+            swap(array,i, smallest);
+        }
+    }
+
+    public static int smallest(int[] array) {
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int min = 0;
+        int smallest = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] <= smallest) {
+                smallest = array[i];
+                min = i;
+            }
+        }
+
+        return min;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int min = table[startIndex];
+        int smallest = 0;
+
+        for (int i = startIndex; i < table.length; i++) {
+            if (table[i] <= min) {
+                min = table[i];
+                smallest = i;
+            }
+        }
+
+        return smallest;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
