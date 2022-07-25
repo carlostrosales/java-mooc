3a9434462809d9b5fcfbc3ba6bbb8897d0d9d839
carlostrosales <rosales.carlost@gmail.com>
2022-07-25 11:13:31 -0500

    Completed 3 more exercises that test knowledge from part I of the course

---
+++ mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            if (input > 0) {
+                sum = sum + input;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+
+            double ans = (double) sum / count;
+
+            System.out.println(ans);
+        }
+
+    }
+}
